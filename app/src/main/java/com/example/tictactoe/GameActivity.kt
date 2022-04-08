2e1f9f059a1f49adf769195d91bafb2475fb1dd8
Niranjan Nayak <niranjannayak0717@gmail.com>
2022-04-08 22:30:35 +0530

    Changes in UI and Color Resources with some validy checks

--- app/src/main/java/com/example/tictactoe/GameActivity.kt
+++ app/src/main/java/com/example/tictactoe/GameActivity.kt
@@ -3,29 +3,34 @@ package com.example.tictactoe
 import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Intent
-import android.nfc.Tag
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.Window
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
 import kotlinx.android.synthetic.main.activity_game.*
-import kotlinx.android.synthetic.main.activity_start.*
 import kotlinx.android.synthetic.main.dialogbox.*
-import kotlin.math.log
 
 class GameActivity : AppCompatActivity(), View.OnClickListener {
 
+    companion object {
+        const val KEY_1 = "player1"
+        const val KEY_2 = "player2"
+    }
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        val player1Name = intent.getStringExtra("player1")
-        val player2Name = intent.getStringExtra("player2")
+        val player1Name = intent.getStringExtra(KEY_1)
+        val player2Name = intent.getStringExtra(KEY_2)
 
-        tvGameBattle.text ="$player1Name v/s $player2Name"
-        tvPlayer.text = "$player1Name's Turn"
+
+        tvGameBattle.text = "$player1Name v/s $player2Name"
+        "$player1Name's Turn".also { tvPlayer.text = it }
 
 //        calling function onClick whenever the button is clicked
 
@@ -39,18 +44,27 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
         btn8.setOnClickListener(this)
         btn9.setOnClickListener(this)
 
-        btnNewGame.setOnClickListener(){
+        btnNewGame.setOnClickListener() {
             finish()
             val newGame = Intent(this, StartActivity::class.java)
             startActivity(newGame)
         }
+
+        connectMail.setOnClickListener() {
+            val mail = Intent()
+            mail.action = Intent.ACTION_SENDTO
+            mail.data = Uri.parse("mailto: niranjannayak0717@gmail.com")
+            startActivity(mail)
+        }
     }
-    private var counter =1
+
+    private var counter = 1
     private val player1 = ArrayList<Int>()
     private val player2 = ArrayList<Int>()
 
+
     @SuppressLint("SetTextI18n")
-    override fun onClick(view: View?){
+    override fun onClick(view: View?) {
 //      getting player names
         val player1Name = intent.getStringExtra("player1")
         val player2Name = intent.getStringExtra("player2")
@@ -58,73 +72,72 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
         val btnClicked = view as Button
         val cellId = btnClicked.tag.toString().toInt()
 
-        if(counter%2!=0){
+        if (counter % 2 != 0) {
             btnClicked.text = "X"
             btnClicked.setBackgroundResource(R.drawable.player1btn)
             player1.add(cellId)
 
-                if(counter>4 && checkPlayer1Win()) {
-                    val show = Dialog(this)
-                    show.requestWindowFeature(Window.FEATURE_NO_TITLE)
-                    show.setCancelable(false)
-                    show.setContentView(R.layout.dialogbox)
-                    show.tvWinner.text="Congratulations!!!\n$player1Name won the game"
-                    show.btnExit.setOnClickListener(){
-                        finish()
-                    }
-                    show.btnPlayAgain.setOnClickListener(){
-                        finish()
-                        val intent = Intent(this,GameActivity::class.java)
-                        intent.putExtra("player1",player1Name)
-                        intent.putExtra("player2", player2Name)
-                        startActivity(intent)
-                   }
-                    show.show()
-                    return
+            if (counter > 4 && checkPlayer1Win()) {
+                val show = Dialog(this)
+                show.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                show.setCancelable(false)
+                show.setContentView(R.layout.dialogbox)
+                show.tvWinner.text = "Congratulations!!!\n$player1Name won the game"
+                show.btnExit.setOnClickListener() {
+                    finish()
                 }
+                show.btnPlayAgain.setOnClickListener() {
+                    finish()
+                    val intent = Intent(this, GameActivity::class.java)
+                    intent.putExtra("player1", player1Name)
+                    intent.putExtra("player2", player2Name)
+                    startActivity(intent)
+                }
+                show.show()
+                return
+            }
             tvPlayer.text = "$player2Name's Turn"
             tvPlayer.setBackgroundResource(R.drawable.player2btn)
-        }
-        else{
-            btnClicked.text ="0"
+        } else {
+            btnClicked.text = "0"
             btnClicked.setBackgroundResource(R.drawable.player2btn)
             player2.add(cellId)
 
-                if(counter>4 && checkPlayer2Win()) {
-                    val show = Dialog(this)
-                    show.requestWindowFeature(Window.FEATURE_NO_TITLE)
-                    show.setCancelable(false)
-                    show.setContentView(R.layout.dialogbox)
-                    show.tvWinner.text="Congratulations!!!\n$player2Name won the game"
-                    show.btnExit.setOnClickListener(){
-                        finish()
-                    }
-                    show.btnPlayAgain.setOnClickListener(){
-                        val intent = Intent(this,GameActivity::class.java)
-                        intent.putExtra("player1",player1Name)
-                        intent.putExtra("player2", player2Name)
-                        startActivity(intent)
-                    }
-                    show.show()
-                    return
+            if (counter > 4 && checkPlayer2Win()) {
+                val show = Dialog(this)
+                show.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                show.setCancelable(false)
+                show.setContentView(R.layout.dialogbox)
+                show.tvWinner.text = "Congratulations!!!\n$player2Name won the game"
+                show.btnExit.setOnClickListener() {
+                    finish()
                 }
+                show.btnPlayAgain.setOnClickListener() {
+                    val intent = Intent(this, GameActivity::class.java)
+                    intent.putExtra("player1", player1Name)
+                    intent.putExtra("player2", player2Name)
+                    startActivity(intent)
+                }
+                show.show()
+                return
+            }
             tvPlayer.text = "$player1Name's Turn"
             tvPlayer.setBackgroundResource(R.drawable.player1btn)
         }
         btnClicked.isEnabled = false
-        if (counter==9) {
+        if (counter == 9) {
             val show = Dialog(this)
             show.requestWindowFeature(Window.FEATURE_NO_TITLE)
             show.setCancelable(false)
             show.setContentView(R.layout.dialogbox)
-            show.tvWinner.text="That was tough to decide Winner\nGame is Draw"
-            show.btnExit.setOnClickListener(){
+            show.tvWinner.text = "That was tough to decide Winner\nGame is Draw"
+            show.btnExit.setOnClickListener() {
                 finish()
             }
-            show.btnPlayAgain.setOnClickListener(){
+            show.btnPlayAgain.setOnClickListener() {
                 finish()
-                val intent = Intent(this,GameActivity::class.java)
-                intent.putExtra("player1",player1Name)
+                val intent = Intent(this, GameActivity::class.java)
+                intent.putExtra("player1", player1Name)
                 intent.putExtra("player2", player2Name)
                 startActivity(intent)
             }
@@ -133,6 +146,7 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
         }
         counter++
     }
+
     private fun checkPlayer1Win(): Boolean {
         if (player1.contains(1) && player1.contains(4) && player1.contains(7))
             return true
@@ -150,10 +164,10 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
             return true
         if (player1.contains(1) && player1.contains(5) && player1.contains(9))
             return true
-        return  false
+        return false
     }
 
-    private fun checkPlayer2Win():Boolean{
+    private fun checkPlayer2Win(): Boolean {
         if (player2.contains(1) && player2.contains(4) && player2.contains(7))
             return true
         if (player2.contains(2) && player2.contains(5) && player2.contains(8))
@@ -170,6 +184,6 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
             return true
         if (player2.contains(1) && player2.contains(5) && player2.contains(9))
             return true
-        return  false
+        return false
     }
 }
--- app/src/main/java/com/example/tictactoe/StartActivity.kt
+++ app/src/main/java/com/example/tictactoe/StartActivity.kt
@@ -1,8 +1,10 @@
 package com.example.tictactoe
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tictactoe.GameActivity.Companion.KEY_1
+import com.example.tictactoe.GameActivity.Companion.KEY_2
 import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
@@ -10,12 +12,20 @@ class StartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
+        etPlayer1.requestFocus()
         btnStartGame.setOnClickListener() {
+            val player1 = etPlayer1.text.toString()
+            val player2 = etPlayer2.text.toString()
+            if (player1.isNotEmpty() && player2.isNotEmpty()) {
                 val intent = Intent(this, GameActivity::class.java)
-                intent.putExtra("player1", etPlayer1.text.toString())
-                intent.putExtra("player2", etPlayer2.text.toString())
+                intent.putExtra(KEY_1, player1)
+                intent.putExtra(KEY_2, player2)
                 startActivity(intent)
                 finish()
+            } else if (player1.isEmpty())
+                etPlayer1.requestFocus()
+            else
+                etPlayer2.requestFocus()
         }
     }
 }
